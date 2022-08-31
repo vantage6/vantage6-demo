9cd115ee9b965534ba6d9e6afdf015b2bf9be0c1
Frank Martin <f.martin@iknl.nl>
2022-08-31 17:03:58 +0200

    Start in detached mode

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 ${HOME}/venvs/vantage6/bin/vnode start --name demo
-docker run -p 80:8000 -v vantage6-demo-user-vol:/mnt harbor2.vantage6.ai/demo/ui
+docker run -p 80:8000 -v vantage6-demo-user-vol:/mnt -d harbor2.vantage6.ai/demo/ui
 echo 'http://localhost'
