317e3c3504cf03b86e79942b9adfb788b840445e
Frank Martin <f.martin@iknl.nl>
2022-09-01 09:59:51 +0200

    automatically start everything

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,7 @@
+# Start the node
 ${HOME}/venvs/vantage6/bin/vnode start --name demo
+
+# Start the node and couple the node volume (which contains data.csv) to this
+# UI app. The app will update this data.csv when the user inputs its values
 docker run -p 80:8000 -v vantage6-demo-user-vol:/mnt -d harbor2.vantage6.ai/demo/ui
-echo 'http://localhost'
+chromium --kiosk http://localhost
